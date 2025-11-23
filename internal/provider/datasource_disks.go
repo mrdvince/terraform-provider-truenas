7da454d655bfdab49ee2234f9e04684e93cf6c85
Vince <vincivenv@gmail.com>
2025-11-23 22:56:31 +0100

    feat: add pool resource and disks data source

---
+++ internal/provider/datasource_disks.go
@@ -0,0 +1,140 @@
+package provider
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"truenas/internal/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var _ datasource.DataSource = &disksDataSource{}
+
+func NewDisksDataSource() datasource.DataSource {
+	return &disksDataSource{}
+}
+
+type disksDataSource struct {
+	client *client.Client
+}
+
+type disksDataSourceModel struct {
+	ID    types.String   `tfsdk:"id"`
+	Ids   []types.String `tfsdk:"ids"`
+	Disks []diskModel    `tfsdk:"disks"`
+}
+
+type diskModel struct {
+	Name   types.String `tfsdk:"name"`
+	Serial types.String `tfsdk:"serial"`
+	Size   types.Int64  `tfsdk:"size"`
+	Type   types.String `tfsdk:"type"`
+}
+
+func (d *disksDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_disks"
+}
+
+func (d *disksDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Retrieves a list of available (unused) disks.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+			},
+			"ids": schema.ListAttribute{
+				ElementType:         types.StringType,
+				Computed:            true,
+				MarkdownDescription: "List of disk names (e.g. sdb, sdc).",
+			},
+			"disks": schema.ListNestedAttribute{
+				Computed: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"name": schema.StringAttribute{
+							Computed: true,
+						},
+						"serial": schema.StringAttribute{
+							Computed: true,
+						},
+						"size": schema.Int64Attribute{
+							Computed: true,
+						},
+						"type": schema.StringAttribute{
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *disksDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected datasource configure type",
+			fmt.Sprintf("Expected *client.Client, got: %T", req.ProviderData),
+		)
+		return
+	}
+
+	d.client = client
+}
+
+func (d *disksDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data disksDataSourceModel
+
+	apiResp, err := d.client.Call(ctx, "disk.get_unused", nil)
+	if err != nil {
+		resp.Diagnostics.AddError("Client error", fmt.Sprintf("Unable to query unused disks: %s", err))
+		return
+	}
+
+	var disks []map[string]any
+	if err := json.Unmarshal(apiResp.Result, &disks); err != nil {
+		resp.Diagnostics.AddError("Client error", "Unable to parse disk query response")
+		return
+	}
+
+	var ids []types.String
+	var diskModels []diskModel
+
+	for _, disk := range disks {
+		name := disk["name"].(string)
+		serial := ""
+		if s, ok := disk["serial"].(string); ok {
+			serial = s
+		}
+		size := int64(0)
+		if s, ok := disk["size"].(float64); ok {
+			size = int64(s)
+		}
+		dtype := ""
+		if t, ok := disk["type"].(string); ok {
+			dtype = t
+		}
+
+		ids = append(ids, types.StringValue(name))
+		diskModels = append(diskModels, diskModel{
+			Name:   types.StringValue(name),
+			Serial: types.StringValue(serial),
+			Size:   types.Int64Value(size),
+			Type:   types.StringValue(dtype),
+		})
+	}
+
+	data.ID = types.StringValue("disks")
+	data.Ids = ids
+	data.Disks = diskModels
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
---
+++ internal/provider/resource_pool.go
@@ -0,0 +1,294 @@
+package provider
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"truenas/internal/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var _ resource.Resource = &poolResource{}
+
+func NewPoolResource() resource.Resource {
+	return &poolResource{}
+}
+
+type poolResource struct {
+	client *client.Client
+}
+
+type poolResourceModel struct {
+	ID       types.String   `tfsdk:"id"`
+	Name     types.String   `tfsdk:"name"`
+	Topology *topologyModel `tfsdk:"topology"`
+}
+
+type topologyModel struct {
+	Data []vdevModel `tfsdk:"data"`
+}
+
+type vdevModel struct {
+	Type  types.String   `tfsdk:"type"`
+	Disks []types.String `tfsdk:"disks"`
+}
+
+func (r *poolResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_pool"
+}
+
+func (r *poolResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "manages a truenas storage pool.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+			},
+			"name": schema.StringAttribute{
+				Required: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+		},
+		Blocks: map[string]schema.Block{
+			"topology": schema.SingleNestedBlock{
+				Blocks: map[string]schema.Block{
+					"data": schema.ListNestedBlock{
+						NestedObject: schema.NestedBlockObject{
+							Attributes: map[string]schema.Attribute{
+								"type": schema.StringAttribute{
+									Required:            true,
+									MarkdownDescription: "vdev type: stripe, mirror, raidz1, raidz2, raidz3",
+									PlanModifiers: []planmodifier.String{
+										stringplanmodifier.RequiresReplace(),
+									},
+								},
+								"disks": schema.ListAttribute{
+									ElementType: types.StringType,
+									Required:    true,
+									PlanModifiers: []planmodifier.List{
+										listplanmodifier.RequiresReplace(),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r *poolResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected resource configure type",
+			fmt.Sprintf("Expected *client.Client, got: %T", req.ProviderData),
+		)
+		return
+	}
+
+	r.client = client
+}
+
+func (r *poolResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data poolResourceModel
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// construct api payload
+	topology := map[string]any{}
+	if data.Topology != nil {
+		var dataVdevs []any
+		for _, vdev := range data.Topology.Data {
+			disks := []string{}
+			for _, d := range vdev.Disks {
+				disks = append(disks, d.ValueString())
+			}
+
+			dataVdevs = append(dataVdevs, map[string]any{
+				"type":  strings.ToUpper(vdev.Type.ValueString()),
+				"disks": disks,
+			})
+		}
+		topology["data"] = dataVdevs
+	}
+
+	params := []any{
+		map[string]any{
+			"name":     data.Name.ValueString(),
+			"topology": topology,
+		},
+	}
+
+	apiResp, err := r.client.Call(ctx, "pool.create", params)
+	if err != nil {
+		resp.Diagnostics.AddError("Client error", fmt.Sprintf("Unable to create pool: %s", err))
+		return
+	}
+
+	// check if response is a job ID (int) and wait for it
+	var jobID int64
+	if err := json.Unmarshal(apiResp.Result, &jobID); err == nil {
+		if err := r.client.WaitForJob(ctx, jobID); err != nil {
+			resp.Diagnostics.AddError("Client error", fmt.Sprintf("Pool creation job failed: %s", err))
+			return
+		}
+	}
+
+	data.ID = data.Name
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *poolResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data poolResourceModel
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	filters := []any{
+		[]any{"name", "=", data.Name.ValueString()},
+	}
+
+	apiResp, err := r.client.Call(ctx, "pool.query", []any{filters})
+	if err != nil {
+		resp.Diagnostics.AddError("Client error", fmt.Sprintf("Unable to read pool: %s", err))
+		return
+	}
+
+	// parse response. it should be a list of pools.
+	var pools []map[string]any
+	if err := json.Unmarshal(apiResp.Result, &pools); err != nil {
+		resp.Diagnostics.AddError("Client error", "Unable to parse pool query response")
+		return
+	}
+
+	if len(pools) == 0 {
+		// pool not found, remove from state
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	pool := pools[0]
+	data.ID = types.StringValue(pool["name"].(string))
+	data.Name = types.StringValue(pool["name"].(string))
+
+	if topology, ok := pool["topology"].(map[string]any); ok {
+		if dataVdevs, ok := topology["data"].([]any); ok {
+			var vdevs []vdevModel
+			for _, v := range dataVdevs {
+				vdevMap, ok := v.(map[string]any)
+				if !ok {
+					continue
+				}
+
+				vdevType := ""
+				if t, ok := vdevMap["type"].(string); ok {
+					vdevType = t
+				}
+
+				var disks []types.String
+				if children, ok := vdevMap["children"].([]any); ok {
+					for _, c := range children {
+						if childMap, ok := c.(map[string]any); ok {
+							if disk, ok := childMap["disk"].(string); ok {
+								disks = append(disks, types.StringValue(disk))
+							}
+						}
+					}
+				}
+
+				vdevs = append(vdevs, vdevModel{
+					Type:  types.StringValue(vdevType),
+					Disks: disks,
+				})
+			}
+
+			if len(vdevs) > 0 {
+				data.Topology = &topologyModel{
+					Data: vdevs,
+				}
+			}
+		}
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *poolResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	// TODO: implement pool updates (adding vdevs, changing properties)
+}
+
+func (r *poolResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data poolResourceModel
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// get numeric id
+	filters := []any{
+		[]any{"name", "=", data.Name.ValueString()},
+	}
+	apiResp, err := r.client.Call(ctx, "pool.query", []any{filters})
+	if err != nil {
+		resp.Diagnostics.AddError("Client error", fmt.Sprintf("Unable to find pool for deletion: %s", err))
+		return
+	}
+
+	// call pool.export (requires numeric id)
+	var pools []map[string]any
+	if err := json.Unmarshal(apiResp.Result, &pools); err != nil {
+		resp.Diagnostics.AddError("Client error", "Unable to parse pool query response")
+		return
+	}
+
+	if len(pools) == 0 {
+		// pool already gone
+		return
+	}
+
+	poolID := pools[0]["id"]
+	// call pool.export(id, options)
+	exportResp, err := r.client.Call(ctx, "pool.export", []any{poolID, map[string]any{"destroy": true, "cascade": true}})
+	if err != nil {
+		resp.Diagnostics.AddError("Client error", fmt.Sprintf("Unable to export/destroy pool: %s", err))
+		return
+	}
+
+	// check if response is a job id (int)
+	var jobID int64
+	if err := json.Unmarshal(exportResp.Result, &jobID); err == nil {
+		// it's a job, wait for it
+		if err := r.client.WaitForJob(ctx, jobID); err != nil {
+			resp.Diagnostics.AddError("Client error", fmt.Sprintf("Pool deletion job failed: %s", err))
+			return
+		}
+	}
+}
+
+func (r *poolResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("name"), req, resp)
+}
