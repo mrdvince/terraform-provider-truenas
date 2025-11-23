a15d34ba1477c29c8d220259c5050e1717005cd6
Vince <vincivenv@gmail.com>
2025-11-24 00:20:11 +0100

    chore: update examples with by_serial output and force_recreate

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     truenas = {
-      source = "registry.terraform.io/vince/truenas"
+      source = "registry.terraform.io/mrdvince/truenas"
     }
   }
 }
@@ -16,12 +16,17 @@ output "available_disks" {
   value = data.truenas_disks.all.ids
 }
 
+output "disks_by_serial" {
+  value = data.truenas_disks.all.by_serial
+}
+
 resource "truenas_pool" "tank" {
   name = "tank"
+  force_recreate = true
   topology {
     data {
       type  = "STRIPE"
-      disks = [data.truenas_disks.all.ids[0]] # dynamically selects the first available disk
+      disks = [data.truenas_disks.all.ids[0]]
     }
   }
 }
